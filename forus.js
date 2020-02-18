1c32f746207fc100fd1045c4589db96363f51e66
parksontano <tanoparksonsilencer@gmail.com>
2020-02-18 11:53:46 +0100

    hey

--- forus.js
+++ forus.js
@@ -1,9 +1,18 @@
-function validateForm()
-{
-    var nam = document.forms["regForm"]["username"].value;
-    if (nam == "")
-    {
-        alert("Enter a valid username")
-        return false;
+function validateForm(){
+    var name = document.forms["regForm"]["username"].value;
+    var email = document.forms["regForm"]["email"].value;
+    var phone = document.forms["regForm"]["phone"].value;
+    var pass1 = document.forms["regForm"]["password"].value;
+    var pass2 = document.forms["regForm"]["password2"].value;
+    var term = document.forms["regForm"]["term"].value;
+
+    if (phone.length > 9){
+        msg = "ENTER VALID PHONE";
     }
+    else
+    {
+        return true;
+    }
+    document.getElementById("phoneErr").innerText = msg;
+
 }
