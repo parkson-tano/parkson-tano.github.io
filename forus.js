c331b505ec656757b2bdc0a88e31c33829d9fab4
King Dani <tanoparksonsilencer@gmail.com>
2020-03-31 18:06:22 +0100

    upload update

--- forus.js
+++ forus.js
@@ -19,3 +19,70 @@ function validateForm(){
     }
 
 };
+<select>
+  <optgroup label="Mammals">
+    <option>Cat</option>
+    <option>Dog</option>
+  </optgroup>
+  <optgroup label="Birds">
+    <option>Crow</option>
+    <option>Eagle</option>
+  </optgroup>
+  <optgroup label="Dinosaurs">
+    <option>Raptor</option>
+    <option>Tyrannosaurus</option>
+    <option>Brachiosaurus</option>
+  </optgroup>
+</select>
+<select class="selectpicker">
+  <optgroup label="Picnic">
+    <option>Mustard</option>
+    <option>Ketchup</option>
+    <option>Relish</option>
+  </optgroup>
+  <optgroup label="Camping">
+    <option>Tent</option>
+    <option>Flashlight</option>
+    <option>Toilet Paper</option>
+  </optgroup>
+</select>
+//search
+<select class="selectpicker" data-live-search="true">
+  <option data-tokens="ketchup mustard">Hot Dog, Fries and a Soda</option>
+  <option data-tokens="mustard">Burger, Shake and a Smile</option>
+  <option data-tokens="frosting">Sugar, Spice and all things nice</option>
+</select>
+<select class="selectpicker" data-style="btn-primary">
+  ...
+</select>
+
+<select class="selectpicker" data-style="btn-info">
+  ...
+</select>
+
+<select class="selectpicker" data-style="btn-success">
+  ...
+</select>
+
+<select class="selectpicker" data-style="btn-warning">
+  ...
+</select>
+
+<select class="selectpicker" data-style="btn-danger">
+  ...
+</select>
+<select class="selectpicker" data-width="auto">
+  ...
+</select>
+<select class="selectpicker" data-width="fit">
+  ...
+</select>
+<select class="selectpicker" data-width="100px">
+  ...
+</select>
+<select class="selectpicker" data-width="75%">
+  ...
+</select>
+<select class="selectpicker">
+  <option data-icon="fa-heart">Ketchup</option>
+</select>
