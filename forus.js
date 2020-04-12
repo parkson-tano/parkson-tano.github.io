6c5668d9d277899d1e8a23a977753f8974cb4663
King Dani <tanoparksonsilencer@gmail.com>
2020-04-13 00:59:55 +0100

    update check

--- forus.js
+++ forus.js
@@ -31,7 +31,7 @@ $(function() {
     });
 
     function check_term() {
-        if ($("#term") === true) {
+        if ($("#term").is(':checked')) {
             $("#term_error_message").hide();
         } else {
             $("#term_error_message").html("Please accept our terms and conditions");
@@ -132,69 +132,3 @@ $(function() {
         }
     });
 });
-
-/* var app1 = new Vue({
-    el: '#app-1',
-    data: {
-        message: 'hello there'
-    }
-})
-
-var app2 = new Vue({
-    el: '#app-2',
-    data: {
-        'seen': true
-    }
-})
-
-var app3 = new Vue({
-    el: '#app-3',
-    data: {
-        message: 'the time now is ' + new Date().toLocaleString()
-    }
-})
-
-var app4 = new Vue({
-    el: '#app-4',
-    data: {
-        todos: [
-            { text: 'item 1' },
-            { text: 'item 2' },
-            { text: 'item 3' }
-        ]
-    },
-    methods: {
-        add: function() {
-            app4.todos.push({ text: this.item })
-        }
-    }
-
-})
-
-var app5 = new Vue({
-    el: '#app-5',
-    data: {
-        message: 'hello daniel'
-    },
-
-})
-
-var app6 = new Vue({
-    el: '#app-6',
-    data: {
-        message: 'hellow'
-    },
-    methods: {
-        reverseMessage: function() {
-            this.message = this.message.split('').reverse().join('')
-        }
-    }
-})
-var app7 = new Vue({
-    el: '#app-7',
-    data: {
-        clickThis: function() {
-            alert('hello')
-        }
-    }
-}) */
